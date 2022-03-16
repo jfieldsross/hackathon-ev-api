6b8c4f446c9d5bac60aab94f9585a563e16f2eda
Harry Dulaney <harry.dulaney@gm.com>
2022-03-15 23:44:56 -0400

    Setup API for cloud

--- src/main/java/com/harrydulaney/firebaseauthstarter/controller/UserController.java
+++ src/main/java/com/harrydulaney/firebaseauthstarter/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.harrydulaney.firebaseauthstarter.controller;
 
-import com.harrydulaney.firebaseauthstarter.security.SecurityService;
 import com.harrydulaney.firebaseauthstarter.security.model.UserDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;

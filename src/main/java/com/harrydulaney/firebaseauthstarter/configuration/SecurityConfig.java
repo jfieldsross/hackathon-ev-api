0155ef11c7f40bce6e63df97fe4b46e3af6aeb4e
Harry Dulaney <harry.dulaney@gm.com>
2022-03-15 20:44:50 -0400

    Cors disable

--- src/main/java/com/harrydulaney/firebaseauthstarter/configuration/SecurityConfig.java
+++ src/main/java/com/harrydulaney/firebaseauthstarter/configuration/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity httpSecurity)
             throws Exception {
         httpSecurity
-                .cors().and()
+                .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
